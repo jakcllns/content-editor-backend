cc87c3c0ae5739ba36be47ece2cd1c06366fb9a4
Jake Collins <jakcllns@gmail.com>
2020-12-20 19:25:40 -0500

    Created graphql schema: Established the Content type Established the ContentInput Established the RootQuery Established the RootMutation Initiated schema

---
+++ graphql/schema.js
@@ -0,0 +1,33 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Content {
+        _id: ID!
+        title: String!
+        imageUrls: [String]
+        content: [String!]
+        author: [String!]
+        createdAt: String!
+    }
+
+    input ContentInput {
+        title: String!
+        content: [String!]
+        imageUrl: [String]
+        author: String!
+
+    }
+
+    type RootMutation {
+        publishContent(contentInput: ContentInput): Content!
+    }
+
+    type RootQuery {
+        getPublishedContent: Content!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`)
