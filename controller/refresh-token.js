f23981dbcc8ee6f47b1729a2b72c375a1c18b017
Jake Collins <jakcllns@gmail.com>
2021-01-19 15:00:44 -0500

    Created signout API for clearing cookie and removing refresh-token from database

--- controller/refresh-token.js
+++ controller/refresh-token.js
@@ -15,10 +15,9 @@ exports.postRefreshToken = async (req, res, next) => {
             throw new Error('No Refresh Cookie');
         }
     
-    
         const decodedToken = jwt.verify(token, process.env.REFRESH_SECRET_KEY);
-        const userId = decodedToken.userId;
-        const refreshToken = await Token.findOne({token: token, userId: userId })
+        const userId = decodedToken.id;
+        const refreshToken = await Token.findOne({token: token, user: userId })
         .populate('User');
 
         if(!refreshToken){
@@ -26,7 +25,7 @@ exports.postRefreshToken = async (req, res, next) => {
         }
         const newJwt = jwt.sign(
             {
-                id: userId,
+                userId: userId,
                 email: refreshToken.user.email
             },
             process.env.JWT_SECRET_KEY,
@@ -35,7 +34,6 @@ exports.postRefreshToken = async (req, res, next) => {
 
         const exp = new Date((decodedToken.exp + 60 * 15) * 1000);
 
-        console.log(exp <= Date.now())
         if(exp <= Date.now()){
             const newRefreshToken = jwt.sign(
                 {id: userId},
@@ -67,6 +65,46 @@ exports.postRefreshToken = async (req, res, next) => {
         });
         
     } catch(err) {
+        res.clearCookie('refresh_token');
         next(err);
     }  
 };
+
+exports.postSignOut = async (req, res, next) => {
+    let refreshToken;
+    let userId;
+    try{
+        const cookieArr = req.headers.cookie
+            .split('; ')
+            .map(c => c.split('='));
+        const [refeshCookie] = cookieArr.length > 0 && cookieArr
+            .filter(cookie => cookie[0] === 'refresh_token');
+        
+        const [_, token] = refeshCookie;
+        
+        if(!token){
+            throw new Error('No Refresh Cookie');
+        }
+
+        const decodedToken = jwt.verify(token, process.env.REFRESH_SECRET_KEY);
+        refreshToken = token;
+        userId = decodedToken.id;
+    } catch(err) {
+        res.clearCookie('refresh_token');
+        return next(err);
+    }
+
+    const myToken = await Token.findOne({token: refreshToken, user: userId})
+    
+    if(myToken){
+        myToken.delete();
+    }
+
+    res.clearCookie('refresh_token');
+    
+    return res.status(200).json({
+        result: true
+    });
+    
+    
+}
--- routes/refresh-token.js
+++ routes/refresh-token.js
@@ -3,6 +3,12 @@ const router = require('express').Router();
 const refreshTokenController = require('../controller/refresh-token');
 
 //Post request
+
+router.post(
+    '/signout',
+    refreshTokenController.postSignOut
+);
+
 router.post(
     '/',
     refreshTokenController.postRefreshToken
